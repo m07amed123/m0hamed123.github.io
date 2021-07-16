8183beb2c67a89ea472bb44f5c77a898511f737c
m07amed123 <87532323+m07amed123@users.noreply.github.com>
2021-07-16 15:24:43 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+    <html1>
+      <head>
+          <title>مديحه شوايه</title>
+      </head>
+            <body style="background-color:#3a09c2;">
+            <font style="border:blue;">
+<center>
+
+</font>
+<p style="border:3px; border-style:solid; border-color:#F5F6F7; padding: 1em;">تنبيه هام ...<br>هذا الموقع هزلي وساخر ولا يمت للواقع بأي صله</p>
+<h1 style="border:red;">مرورك عطر</h1>
+<img src="http://pm1.narvii.com/6841/7e4c4fb5cb461e39299c12ab6f8eddb96f4158bav2_00.jpg" width="400" height="250">               
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<h2>مثاؤ انزل تحت</h2>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<h2>كمان شويه</h2>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<h2>شويه كمان ونبي</h2>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<div class="project-container">
+          <iframe 
+          class="content" controls loop onmouseover="this.play()" onmouseout="this.pause();"
+          width="993" height="559" src="https://www.youtube.com/embed/F6LAOpqb_sI"></iframe>
+</div>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+        <br \>
+      <p>are you?</p>
+        
+                    <?php
+                    $FR_name=array("اخويا الفلسطيني","كريم","توينزي الموكوسه","الوكيل","مريم غيبوبه","اخر");
+                    echo "<select name='your name'>";
+                     for ($i=0;$i<count($FR_name);$i++) {
+                         echo "<option value=$FR_name>" . $FR_name[$i] . "</option>";
+                     }
+                    ?>
+</center>
+        </body>
